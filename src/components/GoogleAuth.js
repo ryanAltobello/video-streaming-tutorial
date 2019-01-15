2ebe154b654a64b931ba380ac860347f63cccef2
ryanAltobello <rchap504@gmail.com>
2019-01-15 15:31:54 -0600

    Added Authorization

---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client
+        .init({
+          clientId:
+            "657752833309-9t7d58qol56nhkq44v5eq9pqs64s910f.apps.googleusercontent.com",
+          scope: "email"
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({
+            isSignedIn: this.auth.isSignedIn.get()
+          });
+          this.auth.isSignedIn.listen(this.onAuthChange);
+        });
+    });
+  }
+
+  onAuthChange = () => {
+    this.setState({
+      isSignedIn: this.auth.isSignedIn.get()
+    });
+  };
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I don't know if we're signed in</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in</div>;
+    } else {
+      return <div>I am not signed in</div>;
+    }
+  }
+  render() {
+    return <div>{this.renderAuthButton()}</div>;
+  }
+}
+
+export default GoogleAuth;
